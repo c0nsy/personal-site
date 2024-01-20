08a2a3b660eef061872a647d194bbdfb9e9d086f
Connor Logan <65863220+c0nsy@users.noreply.github.com>
2024-01-20 13:18:13 -0500

    adding dist

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-// vue.config.js
-const { defineConfig } = require("@vue/cli-service");
-
-module.exports = defineConfig({
-  transpileDependencies: true,
-  base: "/personal-site/",
-  publicPath: "./",
-  // outputDir: "dist", // or your desired output directory
-});
