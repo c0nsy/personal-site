13561d15f82e22c8114a2f2f007750c616c8397a
Connor Logan <65863220+c0nsy@users.noreply.github.com>
2024-01-20 12:28:19 -0500

    updatd styling for mobile users

--- src/modules/work/WorkSection.vue
+++ src/modules/work/WorkSection.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="work-section-wrapper">
     <h1>
-      <span class="header-number">02.</span> Work Experience
+      <div class="header-text">
+        <span class="header-number">02. </span> Work Experience
+      </div>
+
       <hr class="section-divider" />
     </h1>
     <div class="work-section-nav">
